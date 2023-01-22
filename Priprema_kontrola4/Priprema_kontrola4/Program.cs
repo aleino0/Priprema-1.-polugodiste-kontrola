afcd9bb8452c5473a02d476eb3b1ce9667b33841
aleino0 <113008053+aleino0@users.noreply.github.com>
2023-01-22 17:41:44 +0100

    Add files via upload

---
+++ Priprema_kontrola4/Priprema_kontrola4/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Priprema_kontrola4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Unesite cijenu bez PDV-a");
+            double bezPDV=Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Unesite cijenu sa PDV-a");
+            double saPDV = Convert.ToDouble(Console.ReadLine());
+
+
+            if (bezPDV > saPDV)
+            {
+                Console.WriteLine("Neispravan unos cijena.");
+            }
+            else
+            {
+                Console.WriteLine("PDV= {0} % ", ((saPDV - bezPDV) / bezPDV * 100));
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
